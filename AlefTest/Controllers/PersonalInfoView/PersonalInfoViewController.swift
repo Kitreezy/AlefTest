9acff26630d79744a3326ab022f6822fb97f7b4a
ArtemRodionov <ostin161@gmail.com>
2025-02-20 15:02:07 +0300

    # PR 2: create custom TextField

--- AlefTest/Controllers/PersonalInfoView/PersonalInfoViewController.swift
+++ AlefTest/Controllers/PersonalInfoView/PersonalInfoViewController.swift
@@ -9,12 +9,42 @@ import UIKit
 
 class PersonalInfoViewController: UIViewController {
 
+    private lazy var textField: CustomInfoTextField = {
+        let textField = CustomInfoTextField()
+        textField.customPlaceholder = "Имя"
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        setupViews()
+        layoutConstraints()
+        configureAppearance()
     }
 
 
+}
+
+extension PersonalInfoViewController {
+    
+    func setupViews() {
+        view.addSubview(textField)
+    }
+    
+    func layoutConstraints() {
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            textField.widthAnchor.constraint(equalToConstant: 300),
+            textField.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
+    func configureAppearance() {
+        view.backgroundColor = .green
+    }
 }
 
--- AlefTest/Controllers/PersonalInfoView/Views/TextField/CustomInfoTextField.swift
+++ AlefTest/Controllers/PersonalInfoView/Views/TextField/CustomInfoTextField.swift
@@ -5,4 +5,76 @@
 //  Created by Artem Rodionov on 20.02.2025.
 //
 
-import Foundation
+import UIKit
+
+class CustomInfoTextField: UITextField {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .black
+        label.alpha = 0.5
+        
+        return label
+    }()
+    
+    private let textPadding = UIEdgeInsets(top: 20, left: 15, bottom: 5, right: 15)
+    
+    var customPlaceholder: String? {
+        didSet {
+            titleLabel.text = customPlaceholder
+            placeholder = ""
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        setupViews()
+        layoutConstraints()
+        configureAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+        setupViews()
+        layoutConstraints()
+        configureAppearance()
+    }
+        
+    private func setup() {
+        font = .systemFont(ofSize: 15, weight: .regular)
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: textPadding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: textPadding)
+    }
+    
+}
+
+
+extension CustomInfoTextField {
+    
+    func setupViews() {
+        addSubview(titleLabel)
+    }
+    
+    func layoutConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8)
+        ])
+    
+    }
+    
+    func configureAppearance() {
+        borderStyle = .roundedRect
+    }
+}
