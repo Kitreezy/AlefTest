7183b6f4104eaec3556780d24085bc33f84e41fd
ArtemRodionov <ostin161@gmail.com>
2025-02-20 11:02:58 +0300

    #first_commit

---
+++ AlefTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlefTest
+//
+//  Created by Artem Rodionov on 18.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
